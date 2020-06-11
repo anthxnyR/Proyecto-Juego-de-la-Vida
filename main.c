5966663ec45396bc62720cbb83ac41f883717c7f
Anthony Barrios <arbarrios.19@est.ucab.edu.ve>
2020-06-11 00:28:36 -0400

    Generadas las otras dos matrices, juego funcional
    También se estableció un prototipo de la función StartGame para evitar usar tanto el main.
    
    Se agregó a los parámetros de cada función la variable idx que nos indicará en qué matriz queremos escribir, esto dependiendo a cual versión queramos.

--- main.c
+++ main.c
@@ -49,9 +49,10 @@ int ***CrearMatriz(char path[]){                     //Funcion que crea la matri
             Mundo[i][j]=(int *)malloc((col+2)*(sizeof(int)));
     }
 
-    for(int i=0;i<(fil+2);i++)
-        for(int j=0;j<(col+2);j++)
-            Mundo[0][i][j]=9;
+    for(int i=0;i<3;i++)
+        for(int j=0;j<(fil+2);j++)
+            for(int k=0;k<(col+2);k++)
+                Mundo[i][j][k]=9;
 
     int j=1,k=1;
     archivo=fopen(path,"r");
@@ -62,6 +63,8 @@ int ***CrearMatriz(char path[]){                     //Funcion que crea la matri
             for(int i=0;i<strlen(str);i++){
                 if(str[i]==48||str[i]==49){
                     Mundo[0][j][k]=str[i]-'0';
+                    Mundo[1][j][k]=str[i]-'0';
+                    Mundo[2][j][k]=str[i]-'0';
                     k++;
                 }
             }
@@ -93,47 +96,138 @@ int ***MatrizAux(char path[]){
             Aux[i][j]=(int *)malloc((col+2)*(sizeof(int)));
     }
 
-    for(int i=0;i<fil+2;i++)
-        for(int j=0;j<col+2;j++)
-            Aux[0][i][j]=9;
+    for(int i=0;i<3;i++)
+        for(int j=0;j<(fil+2);j++)
+            for(int k=0;k<(col+2);k++)
+                Aux[i][j][k]=9;
 
     return Aux;
 }
 
-void SigGeneration(int ***Mundo, int ***Auxiliar,int *dimensiones,int generation){
+int ***NextGenClassic(int ***Mundo, int ***Auxiliar,int *dimensiones,int idx){
     int i,j,alive,fil=dimensiones[0],col=dimensiones[1];
     for(i=1;i<fil+1;i++)
         for(j=1;j<col+1;j++){
             alive=0;
-            if(Mundo[0][i-1][j-1]==1)
+            if(Mundo[idx][i-1][j-1]==1)
                 alive++;
-            if(Mundo[0][i-1][j]==1)
+            if(Mundo[idx][i-1][j]==1)
                 alive++;
-            if(Mundo[0][i-1][j+1]==1)
+            if(Mundo[idx][i-1][j+1]==1)
                 alive++;
-            if(Mundo[0][i][j-1]==1)
+            if(Mundo[idx][i][j-1]==1)
                 alive++;
-            if(Mundo[0][i][j+1]==1)
+            if(Mundo[idx][i][j+1]==1)
                 alive++;
-            if(Mundo[0][i+1][j-1]==1)
+            if(Mundo[idx][i+1][j-1]==1)
                 alive++;
+            if(Mundo[idx][i+1][j]==1)
+                alive++;
+            if(Mundo[idx][i+1][j+1]==1)
+                alive++;
+            if(Mundo[idx][i][j]==1 && (alive<2 || alive>3))
+                Auxiliar[idx][i][j]=0;
+            else if(Mundo[idx][i][j]==0 && alive==3)
+                Auxiliar[idx][i][j]=1;
+                else Auxiliar[idx][i][j]=Mundo[idx][i][j];
+        }
+    return Auxiliar;
+}
+
+int ***NextGenVert(int ***Mundo, int ***Auxiliar, int *dimensiones,int idx){     //Recibe idx que indicara el indice de la matriz que trabaja
+    int i,j,alive,fil=dimensiones[0],col=dimensiones[1];
+    for(i=1;i<fil+1;i++)
+        for(j=1;j<col+1;j++){
+            alive=0;
+            if(Mundo[idx][i-1][j-1]==1)
+                alive++;
+                else if(Mundo[idx][i-1][j-1]==9 && Mundo[idx][fil][j-1]==1)
+                    alive++;
+            if(Mundo[idx][i-1][j]==1)
+                alive++;
+                else if(Mundo[idx][i-1][j]==9 && Mundo[idx][fil][j]==1)
+                    alive++;
+            if(Mundo[idx][i-1][j+1]==1)
+                alive++;
+                else if(Mundo[idx][i-1][j+1]==9 && Mundo[idx][fil][j+1]==1)
+                    alive++;
+            if(Mundo[idx][i][j-1]==1)
+                alive++;
+            if(Mundo[idx][i][j+1]==1)
+                alive++;
+            if(Mundo[idx][i+1][j-1]==1)
+                alive++;
+                else if(Mundo[idx][i+1][j-1]==9 && Mundo[idx][1][j-1]==1)
+                    alive++;
             if(Mundo[0][i+1][j]==1)
                 alive++;
-            if(Mundo[0][i+1][j+1]==1)
+                else if(Mundo[0][i+1][j]==9 && Mundo[0][1][j]==1)
+                    alive++;
+            if(Mundo[idx][i+1][j+1]==1)
                 alive++;
-            if(Mundo[0][i][j]==1 && (alive<2 || alive>3))
-                Auxiliar[0][i][j]=0;
-            else if(Mundo[0][i][j]==0 && alive==3)
-                Auxiliar[0][i][j]=1;
-            else Auxiliar[0][i][j]=Mundo[0][i][j];
+                else if(Mundo[idx][i+1][j+1]==9 && Mundo[idx][1][j+1]==1)
+                    alive++;
+            if(Mundo[idx][i][j]==1 && (alive<2 || alive>3))
+                Auxiliar[idx][i][j]=0;
+                else if(Mundo[idx][i][j]==0 && alive==3)
+                    Auxiliar[idx][i][j]=1;
+                    else
+                        Auxiliar[idx][i][j]=Mundo[0][i][j];
         }
-    printf("Generacion %d\n",generation+1);
-    for(i=1;i<fil+1;i++){
-        for(j=1;j<col+1;j++)
-            printf("%d ",Auxiliar[0][i][j]);
-        printf("\n");
-    }
+    return Auxiliar;
 }
+
+int ***NextGenHor(int ***Mundo, int ***Auxiliar, int *dimensiones,int idx){
+    int i,j,alive,fil=dimensiones[0],col=dimensiones[1];
+    for(i=1;i<fil+1;i++)
+        for(j=1;j<col+1;j++){
+            alive=0;
+            if(Mundo[idx][i-1][j-1]==1)
+                alive++;
+                else if(Mundo[idx][i-1][j-1]==9 && Mundo[idx][i-1][col]==1)
+                    alive++;
+            if(Mundo[idx][i-1][j]==1)
+                alive++;
+            if(Mundo[idx][i-1][j+1]==1)
+                alive++;
+                else if(Mundo[idx][i-1][j+1]==9 && Mundo[idx][i-1][1]==1)
+                    alive++;
+            if(Mundo[idx][i][j-1]==1)
+                alive++;
+                else if(Mundo[idx][i][j-1]==9 && Mundo[idx][i][col]==1)
+                    alive++;
+            if(Mundo[idx][i][j+1]==1)
+                alive++;
+                else if(Mundo[idx][i][j+1]==9 && Mundo[idx][i][1]==1)
+                    alive++;
+            if(Mundo[0][i+1][j-1]==1)
+                alive++;
+                else if(Mundo[idx][i+1][j-1]==9 && Mundo[idx][i+1][col]==1)
+                    alive++;
+            if(Mundo[idx][i+1][j]==1)
+                alive++;
+            if(Mundo[idx][i+1][j+1]==1)
+                alive++;
+                else if(Mundo[idx][i+1][j+1]==9 && Mundo[idx][i+1][1]==1)
+                    alive++;
+            if(Mundo[idx][i][j]==1 && (alive<2 || alive>3))
+                Auxiliar[idx][i][j]=0;
+                else if(Mundo[idx][i][j]==0 && alive==3)
+                    Auxiliar[idx][i][j]=1;
+                    else
+                        Auxiliar[idx][i][j]=Mundo[idx][i][j];
+        }
+    return Auxiliar;
+}
+
+/*void StartGame(char path[],int generation){
+    int i=0,flag=0;
+    int ***Mundo, ***Auxiliar;
+    int *dimensiones=RevisarMatriz(path);
+    Mundo=CrearMatriz(path);
+    Auxiliar=MatrizAux(path);
+
+}*/
 
 int main(){
     int ***Mundo;
@@ -144,14 +238,16 @@ int main(){
     scanf("%d",&gen);
     Mundo=CrearMatriz("prueba.txt");
     Auxiliar=MatrizAux("prueba.txt");
-    while(i<gen){
-        if(flag==0){
-            SigGeneration(Mundo,Auxiliar,dimensiones,i);
-            flag=1;
-        }else if(flag==1){
-            SigGeneration(Auxiliar,Mundo,dimensiones,i);
-            flag=0;
-        }
-        i++;
+    Auxiliar=NextGenClassic(Mundo,Auxiliar,dimensiones,0);
+    Auxiliar=NextGenHor(Mundo,Auxiliar,dimensiones,1);
+
+    for(i=1;i<dimensiones[0]+1;i++){
+        for(int j=1;j<dimensiones[1]+1;j++)
+            printf("%d ",Auxiliar[0][i][j]);
+        printf("   ");
+        for(int j=1;j<dimensiones[1]+1;j++)
+            printf("%d ",Auxiliar[1][i][j]);
+        printf("\n");
     }
+    return 0;
 }
